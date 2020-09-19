c27bfb1f5117a1338520f2c6958ab9ac6a4d1f54
Anastassy Cap <anacap100@gmail.com>
2020-09-19 13:47:33 -0400

    Learning MongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,13 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
@@ -11,8 +16,36 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     }
 
     const db = client.db(databaseName) // Create db
-    db.collection('Users').insertOne({
-        name: 'AC',
-        age: 20
-    })
+
+    // db.collection('Users').insertOne({
+    //     name: 'JC',
+    //     age: 99
+    // }, (error, result) => {
+    //
+    //     if (error){
+    //         return console.log('Unable to insert user...')
+    //     }
+    //
+    //     console.log(result.ops)
+    //
+    // })
+
+    // db.collection('Tasks').insertMany([
+    //     {
+    //     description: "Finish Pen Testing Courses",
+    //     completed: false
+    //     },
+    //     {
+    //         description: "Learn Node.js",
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error){
+    //         return console.log('Unable to insert user...')
+    //     }
+    //
+    //     console.log(result.ops)
+    //
+    // })
+
 })
