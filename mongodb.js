07ac0b4bf663ea79cfabe805bae831332a8bd012
Anastassy Cap <anacap100@gmail.com>
2020-09-20 20:51:23 -0400

    JS promises

--- mongodb.js
+++ mongodb.js
@@ -5,47 +5,28 @@ const ObjectID = mongodb.ObjectID
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
-    if(error){
+    if (error) {
         return console.log('Unable to connect to db...')
     }
 
-    const db = client.db(databaseName) // Create db
+    const db = client.db(databaseName) // Create db or connect to it
 
-    // db.collection('Users').insertOne({
-    //     name: 'JC',
-    //     age: 99
-    // }, (error, result) => {
-    //
-    //     if (error){
-    //         return console.log('Unable to insert user...')
+    // db.collection('Users').findOne({name: 'AC'}, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch user...')
     //     }
-    //
-    //     console.log(result.ops)
-    //
+    //     console.log(user)
     // })
 
-    // db.collection('Tasks').insertMany([
-    //     {
-    //     description: "Finish Pen Testing Courses",
-    //     completed: false
-    //     },
-    //     {
-    //         description: "Learn Node.js",
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error){
-    //         return console.log('Unable to insert user...')
-    //     }
-    //
-    //     console.log(result.ops)
-    //
-    // })
+    db.collection('Users').find({age: 20}).toArray((error, users) => {
+        console.log(users)
+    })
+
+    db.collection('Users').find({age: 20}).count((error, count) => {
+        console.log(count)
+    })
 
 })
